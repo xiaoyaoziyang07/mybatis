8bc36f3f6226b4daff508a80dd294ff9eadb48bd
liyang <742847057@qq.com>
2017-04-05 16:56:21 +0800

    hehe

--- mybatis-study/src/test/java/com/chezhibao/liyang/BaseTest.java
+++ mybatis-study/src/test/java/com/chezhibao/liyang/BaseTest.java
@@ -7,17 +7,39 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BaseTest {
+	
+	private SqlSessionFactory factory;
+	
+	private SqlSession session;
 
-	@Test
+	@BeforeClass
 	public void testSessionFactory() throws IOException{
 		InputStream config = Resources.getResourceAsStream("mybatis-config.xml");
 		SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
-		SqlSessionFactory factory = factoryBuilder.build(config);
+		factory = factoryBuilder.build(config);
+	}
+	
+	@Before
+	public void testSession(){
 		SqlSession session = factory.openSession();
 		Assert.assertNotNull(session);
+	}
+	
+	@Test
+	public void testMapper(){
+		Object selectOne = session.selectOne("org.mybatis.example.BlogMapper.selectBlog", 101);
+	}
+	
+	@After
+	public void closeSession(){
+		session.close();
 	}
 }
